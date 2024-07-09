3507b56785f5472a2f799fa567dd6006850abbc3
Grace <gracecossai@gmail.com>
2024-07-09 10:50:15 +0100

    Adjusted the screen size

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="bg-gray-900 text-gray-100 pb-6 lg:pb-0 h-full lg:h-screen flex flex-col justify-center items-center overflow-hidden">
-      <div className="partial-border p-5 h-full smallBP:h-screen lg:h-screen">
+    <div className="bg-gray-900 text-gray-100 pb-6 lg:pb-0 h-full  flex flex-col justify-center items-center overflow-hidden">
+      <div className="partial-border p-5 h-full smallBP:h-full lg:h-full">
         <Header />
         <main className="mt-10">
           <Home />
