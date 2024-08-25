d10dffde4a82d67ad8aa73901bdffd585b79b8fd
Grace <gracecossai@gmail.com>
2024-08-25 21:34:18 +0100

    Modified my code

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,43 +1,15 @@
 import React from "react";
 import Title from "../Title/Title";
-// import { Link } from 'react-router-dom';
+import itemData from "../itemData";
+
+
 const Projects = ({className}) => {
-  const data = [
-    {
-      projectUrl: "https://mymakeupproject.netlify.app/",
-      projectName: "Make up API Project",
-    },
-    {
-      projectUrl: "https://myspacetouch.netlify.app/",
-      projectName: "Space Touch",
-    },
-    {
-      projectUrl: "https://grace-ecommerce.netlify.app/",
-      projectName: "E-commerce",
-    },
-    {
-      projectUrl: "https://stately-pavlova-99c6ba.netlify.app/",
-      projectName: "D O A",
-    },
-    {
-      projectUrl: "https://shiny-dusk-a340cd.netlify.app/",
-      projectName: "Logistics",
-    },
-    {
-      projectUrl: "https://ecurrencytask.netlify.app/",
-      projectName: "E-currency-Task",
-    },
-    {
-      projectUrl: "https://amsgrace.netlify.app/",
-      projectName: "Attendance Management System",
-    },
-  ];
 
   return (
     <div className={`relative ${className}`}>
       <Title text="Projects" className="mb-2" />
       <div className="projects lg:mt-5">
-        {data.map((project, index) => (
+        {itemData.map((project, index) => (
           <div
             key={index}
             className="lg:mb-2 transform hover:scale-105 transition delay-50 duration-300 ease-in-out"
---
+++ src/components/itemData.js
@@ -0,0 +1,32 @@
+const itemData = [
+  {
+    projectUrl: "https://mymakeupproject.netlify.app/",
+    projectName: "Make up API Project",
+  },
+  {
+    projectUrl: "https://myspacetouch.netlify.app/",
+    projectName: "Space Touch",
+  },
+  {
+    projectUrl: "https://grace-ecommerce.netlify.app/",
+    projectName: "E-commerce",
+  },
+  {
+    projectUrl: "https://stately-pavlova-99c6ba.netlify.app/",
+    projectName: "D O A",
+  },
+  {
+    projectUrl: "https://shiny-dusk-a340cd.netlify.app/",
+    projectName: "Logistics",
+  },
+  {
+    projectUrl: "https://ecurrencytask.netlify.app/",
+    projectName: "E-currency-Task",
+  },
+  {
+    projectUrl: "https://amsgrace.netlify.app/",
+    projectName: "Attendance Management System",
+  },
+];
+
+export default itemData
